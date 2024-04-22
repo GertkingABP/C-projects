966cf0405a2581c0feebc68fe975efa1e889b2d7
Gertking <103680535+GertkingABP@users.noreply.github.com>
2024-04-22 14:22:24 +0300

    Параллельные вычисления на потоках в C#

--- Program.cs
+++ Program.cs
@@ -1,22 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace lorry_db_L7
+class Program
 {
-    static class Program
+    static int[] arr;
+    static int count;
+    static int k;
+
+    public static int CountZeroChains(int[] arr, int k, int numThreads)
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        int count = 0;
+        int len = arr.Length;
+        int chunkSize = len / numThreads;
+
+        object lockObj = new object();
+        Thread[] threads = new Thread[numThreads];
+
+        for (int i = 0; i < numThreads; i++)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int start = i * chunkSize;
+            int end = (i == numThreads - 1) ? len : (i + 1) * chunkSize;
+
+            threads[i] = new Thread(() =>
+            {
+                int localCount = 0;
+
+                for (int j = start; j < end - k; j++)
+                {
+                    bool isChain = true; //является ли текущая последовательность цепочкой из k нулей
+
+                    for (int l = j; l < j + k; l++)
+                    {
+                        if (arr[l] != 0)
+                        {
+                            isChain = false;
+                            break;
+                        }
+                    }
+
+                    if (isChain)
+                        localCount++;
+                }
+
+                lock (lockObj)
+
+                    count += localCount;
+            });
+
+            threads[i].Start();
         }
+
+        for (int i = 0; i < numThreads; i++)
+            threads[i].Join();
+
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        int t = 1;
+        k = 3;
+        int thread_num = 1;
+        //arr = new int[] { 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1 };
+        arr = new int[180180000];
+        Random rand = new Random();
+
+        while (t <= 10)
+        {
+            Console.WriteLine("\n---------------------------------------\nИтерация {0}", t);
+            for (int x = 0; x < arr.Length; x++)
+            {
+                arr[x] = rand.Next(4);
+            }
+
+            for (int i = 0; i < 10; i++)//первые 10 чисел
+                Console.WriteLine("{0} ", arr[i]) ;
+
+            while (thread_num <= 16)
+            {
+
+                stopwatch.Start();
+                count = CountZeroChains(arr, k, thread_num);
+                stopwatch.Stop();
+
+                Console.WriteLine("\n\nРабота в {0} поток(е/ах):", thread_num);
+                Console.WriteLine("\nКоличество цепочек из {0} нулей: {1}", k, count);
+                TimeSpan ts = stopwatch.Elapsed;
+
+                Console.WriteLine("\nВремя вычислений = {0:00}:{1:00}:{2:00}.{3}",
+                                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+                thread_num++;
+                stopwatch.Reset();
+            }
+
+            t++;
+            thread_num = 1;
+        }
+
+        Console.ReadKey();
     }
 }
---
+++ Program1.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+int ComputeLevenshteinDistance(string s, string t)
+{
+    int m = s.Length;
+    int n = t.Length;
+    int[,] d = new int[m + 1, n + 1];
+
+    if (m == 0)
+        return n;
+
+    if (n == 0)
+        return m;
+
+    for (int i = 0; i <= m; i++)
+        d[i, 0] = i;
+
+    for (int j = 0; j <= n; j++)
+        d[0, j] = j;
+
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = 1; i <= m; i++)
+        {
+            int cost = s[i - 1] == t[j - 1] ? 0 : 1;
+            d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
+        }
+    }
+
+    return d[m, n];
+}
+
+string text = File.ReadAllText("text4.txt");
+string pattern = File.ReadAllText("search.txt");
+Stopwatch stopwatch = new Stopwatch();
+int numThreads = 1;
+int t = 1;
+
+while (t <= 10)
+{
+    Console.WriteLine("\n---------------------------------------\nИтерация {0}", t);
+    while (numThreads <= 16)
+    {
+        Console.WriteLine("\n\n\nРабота в {0} поток(е/ах):", numThreads);
+        int chunkSize = text.Length / numThreads;
+        List<string> chunks = new List<string>();
+
+        for (int i = 0; i < numThreads; i++)
+        {
+            int start = i * chunkSize;
+            int end = (i + 1) * chunkSize;
+
+            if (i == numThreads - 1)
+                end = text.Length;
+
+            chunks.Add(text.Substring(start, end - start));
+        }
+
+        string closestMatch = null;
+        int closestMatchDistance = int.MaxValue;
+        object lockObj = new object();
+        List<Thread> threads = new List<Thread>();
+
+        stopwatch.Start();
+
+        Parallel.ForEach(chunks, chunk =>
+        {
+            int startIndex = text.IndexOf(chunk);
+            int endIndex = startIndex + chunk.Length;
+            for (int i = startIndex; i < endIndex - pattern.Length; i++)
+            {
+                string substring = text.Substring(i, pattern.Length);
+                int distance = ComputeLevenshteinDistance(substring, pattern);
+
+                if (distance < closestMatchDistance)
+                {
+                    lock (lockObj)
+                    {
+                        if (distance < closestMatchDistance)
+                        { 
+                            closestMatch = substring;
+                            closestMatchDistance = distance;
+                        }
+                    }
+                }
+            }
+        });
+
+        stopwatch.Stop();
+
+        TimeSpan ts = stopwatch.Elapsed;
+        Console.WriteLine("\nСлучай с Parallel.ForEach:");
+        Console.WriteLine("\nНаиболее похожая строка: {0}", closestMatch);
+        Console.WriteLine("\nНаименьшее расстояние Левенштейна: {0}", closestMatchDistance);
+        Console.WriteLine("\nВремя вычислений = {000:00}.{1}",
+                                ts.Seconds, ts.Milliseconds);
+        stopwatch.Reset();
+        numThreads++;
+    }
+
+    numThreads = 1;
+    t++;
+}
